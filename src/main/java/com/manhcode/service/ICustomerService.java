564f8b9041063b52565b96e73d97057c4c234b54
tranthemanh <tranmanhhuha@gmail.com>
2024-08-16 13:59:48 +0700

    lan 1

---
+++ src/main/java/com/manhcode/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.manhcode.service;
+
+import com.manhcode.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
+}
